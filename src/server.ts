2a457fadbe32d25d61863ff62d5866eae9330f91
eliveltonsf <elivelton.seven@gmail.com>
2024-05-02 16:29:55 -0300

    build: add prisma dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
-import fastifyCors from '@fastify/cors';
-import fastify from "fastify";
-import { teste } from './routes/teste';
-
 import 'dotenv/config';
 import packageJson from "../package.json";
 
-const app = fastify()
+import fastifyCors from '@fastify/cors';
+import fastify from "fastify";
 
+import { ZodTypeProvider, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { teste } from './routes/teste';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: '*'
 })
 
-app.register(teste)
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
+app.register(teste)
 
 const port = Number(process.env.PORT)
 
