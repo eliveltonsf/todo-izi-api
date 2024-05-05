0b26cbf3a77e25c36cda2b83b95a89e62e910223
eliveltonsf <elivelton.seven@gmail.com>
2024-05-05 02:01:37 -0300

    feat: add controllers from list and update task

--- src/modules/task/task-controller.ts
+++ src/modules/task/task-controller.ts
@@ -1,28 +1,82 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { createTask } from "./task-service";
+import { createTask, getTasks, updateTask } from "./task-service";
 
 import { decodeTokenJWT } from "../../schemas";
-import { taskSchema } from './task-schema';
+import { generateSafeParse } from "../../util/generateSafeParse";
+import { paramTaskSchema, taskSchema, updateTaskSchema } from "./task-schema";
 
-export async function createTaskHandler (
+export async function createTaskHandler(
   request: FastifyRequest,
   reply: FastifyReply
-){
-  const decodeToken = decodeTokenJWT.safeParse(request.user);
+) {
+  const userId = await generateSafeParse(
+    request,
+    reply,
+    decodeTokenJWT,
+    "Enter the access token properly.",
+    "user"
+  );
 
-  if(!decodeToken.success){
-    return reply.status(401).send('Enter the access token properly.');
-  }  
+  const bodyParsed = await generateSafeParse(
+    request,
+    reply,
+    taskSchema,
+    "Check that all information is correct.",
+    "body"
+  );
 
-  const userId = decodeToken.data.sub
+  const task = await createTask({
+    ...bodyParsed,
+    userId: userId.sub,
+  });
 
-  const bodyParsed = taskSchema.safeParse(request.body)
+  return reply.status(201).send(task);
+}
 
-  if(!bodyParsed.success){
-    return reply.status(401).send('Check that all information is correct');
-  }
+export async function getTasksHandler(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const userId = await generateSafeParse(
+    request,
+    reply,
+    decodeTokenJWT,
+    "Enter the access token properly.",
+    "user"
+  );
 
-  const task = await createTask({...bodyParsed.data, userId});
+  const products = await getTasks(userId.sub);
+
+  return products;
+}
+
+export async function updateTaskHandler(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const bodyParsed = await generateSafeParse(
+    request,
+    reply,
+    updateTaskSchema,
+    "Check that all information is correct.",
+    "body"
+  );
+
+  const idTask = await generateSafeParse(
+    request,
+    reply,
+    paramTaskSchema,
+    "Check that all information is correct.",
+    "params"
+  );
+
+  const task = await updateTask({
+    ...bodyParsed,
+    id: Number(idTask.id),
+    updatedAt: new Date(),
+  });
+
+  console.log("response", task);
 
   return reply.status(201).send(task);
 }
