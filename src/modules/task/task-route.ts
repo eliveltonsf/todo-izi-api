59e3f1e7c27a2b12d0332202f17f6986e8c4cc5d
eliveltonsf <elivelton.seven@gmail.com>
2024-05-05 02:00:20 -0300

    feat: add routes from list and update task

--- src/modules/task/task-route.ts
+++ src/modules/task/task-route.ts
@@ -1,8 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { verifyJwt } from "../user/user-controller";
-import { createTaskHandler } from "./task-controller";
-import { taskResponseSchema, taskSchema } from "./task-schema";
+import { createTaskHandler, getTasksHandler, updateTaskHandler } from "./task-controller";
+import { paramTaskSchema, taskResponseSchema, taskSchema, tasksResponseSchema, updateTaskResponseSchema, updateTaskSchema } from "./task-schema";
 
 
 export async function taskRoutes(app: FastifyInstance) {
@@ -17,6 +17,32 @@ export async function taskRoutes(app: FastifyInstance) {
     }
   },
     createTaskHandler   
+  )
+
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/",
+    {
+      onRequest: [verifyJwt],
+      schema: {
+        response: {
+          200: tasksResponseSchema
+        },
+      },
+    },
+    getTasksHandler
+  );
+
+  app.withTypeProvider<ZodTypeProvider>().patch('/:id', {
+    onRequest: [verifyJwt],
+    schema:{
+      body: updateTaskSchema,
+      params: paramTaskSchema,
+      response: {
+        201: updateTaskResponseSchema
+      },      
+    }
+  },
+    updateTaskHandler   
   )
 
 }
