e85982d56d3bacdc7dffb8fc8fa68e10af1d994a
eliveltonsf <elivelton.seven@gmail.com>
2024-05-05 02:01:12 -0300

    feat: add schema the zod for update task

--- src/modules/task/task-schema.ts
+++ src/modules/task/task-schema.ts
@@ -3,7 +3,7 @@ import z from "zod";
 const taskType = {
   title: z.string(),
   description: z.string(),
-  status: z.boolean().default(false)
+  status: z.boolean()
 };
 
 const taskGenerated = {
@@ -11,6 +11,20 @@ const taskGenerated = {
   createdAt: z.date(),
   updatedAt: z.date().nullish(),
 };
+
+const updateTaskType = {
+  title: z.string().optional(),
+  description: z.string().optional(),
+  status: z.boolean().optional()   
+};
+
+const updateTaskGenerated = {
+  id: z.number(),
+  createdAt: z.date(),
+  updatedAt: z.date()  
+};
+
+
 
 export const taskSchema = z.object({
   ...taskType,
@@ -26,6 +40,21 @@ export const taskResponseSchema = z.object({
   ...taskGenerated,
 });
 
+export const updateTaskSchema = z.object({
+  ...updateTaskType,
+  updatedAt: z.date().optional()
+});
+
+export const updateTaskResponseSchema = z.object({
+  ...updateTaskType,
+  ...updateTaskGenerated,
+});
+
+export const paramTaskSchema = z.object({
+  id: z.string()
+});
+
 export const tasksResponseSchema = z.array(taskResponseSchema);
 
 export type CreateTaskType = z.infer<typeof createTaskSchema>;
+export type UpdateTaskType = z.infer<typeof updateTaskSchema>;
